0312b2de2295016ebfaf5859291860f623524915
Владислав <lusta.forsocial@gmail.com>
2026-04-28 21:10:37 +0300

    fix: types

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,13 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  // Точка входа в вашу библиотеку
-  entry: ['src/index.ts'],
-  // Целевые форматы: ES Modules и CommonJS
-  format: ['esm', 'cjs'],
-  // Генерировать файлы деклараций типов (.d.ts)
-  dts: false,
-  // Очищать выходную директорию перед сборкой
+  entry: ['src/index.ts'],   // только точка входа
+  format: ['cjs', 'esm'],
+  dts: true,                 // генерирует index.d.ts
   clean: true,
-  // Минификация кода (опционально, можно отключить для отладки)
-  minify: false,
-  // Путь для выходных файлов
   outDir: 'dist',
-  // Удалить все вызовы console.*
+  sourcemap: true,
   esbuildOptions(options) {
-    options.drop = ['console']; // удаляет все console.*
+    options.drop = ['console'];
   },
 });
